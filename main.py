d459f9565254114f0a13aee58eb8a20f567c4eca
wimthomzik <82528324+wimthomzik@users.noreply.github.com>
2026-06-19 10:23:57 -0700

    update player in loop

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ def main():
     pygame.init()
     
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     clock = pygame.time.Clock()
     dt = 0.0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         log_state()    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
 
 if __name__ == "__main__":
     main()
