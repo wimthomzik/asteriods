6a7b8ca21d6abbc6a336b2852ca816a346a11965
wimthomzik <82528324+wimthomzik@users.noreply.github.com>
2026-06-19 10:00:11 -0700

    add stabilization at 60fps

--- main.py
+++ main.py
@@ -10,13 +10,17 @@ def main():
     
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0.0
+        
     while True:
-        log_state()
+        log_state()    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
