c4656c90401d384c325c9c93b80f12e0cbe70932
wimthomzik <82528324+wimthomzik@users.noreply.github.com>
2026-06-19 10:12:33 -0700

    continuosly draw player on screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -12,13 +13,14 @@ def main():
     
     clock = pygame.time.Clock()
     dt = 0.0
-        
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
