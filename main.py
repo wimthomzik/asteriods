401f1c1750af1ebf8d4ce80d7ace38bc6e68e864
wimthomzik <82528324+wimthomzik@users.noreply.github.com>
2026-06-22 10:32:35 -0700

    kill shot and asteriod after collision

--- main.py
+++ main.py
@@ -39,6 +39,12 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
         for d in drawable:
             d.draw(screen)
         pygame.display.flip()
